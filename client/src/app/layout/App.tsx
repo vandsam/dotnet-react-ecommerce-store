3dee0c5573c7bfa2dd40b6dd46151dbeaf023024
Sam Vanderlinden <samvanderlinden6@gmail.com>
2023-02-14 16:09:43 -0600

    add Header component, update list to Cards

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import './styles.css';
-import { Product } from '../models/product';
-import Catalog from '../../features/catalog/Catalog';
-import { Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
 
   function addProduct() {
-    setProducts(prevState => [...prevState, 
+    setProducts((prevState) => [
+      ...prevState,
       {
         id: prevState.length + 101,
-        name: "product" + (prevState.length + 1), 
+        name: "product" + (prevState.length + 1),
         price: (prevState.length + 1) * 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: `http://picsum.photos/${200 + prevState.length}`
-      }
-    ])
+        brand: "some brand",
+        description: "some description",
+        pictureUrl: `http://picsum.photos/${200 + prevState.length}`,
+      },
+    ]);
   }
 
   useEffect(() => {
     async function fetchData() {
       // You can await here
-      const response = await fetch('http://localhost:5264/api/products');
+      const response = await fetch("http://localhost:5264/api/products");
       const data = await response.json();
-      setProducts(data)
+      setProducts(data);
       // ...
     }
     fetchData();
   }, []); // Or [] if effect doesn't need props or state
-  
+
   return (
     <>
-      <Typography variant='h1'>eCommerce Store</Typography>
-      <Catalog products={products} addProduct={addProduct}/>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
     </>
   );
 }
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,11 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import {
+  CardMedia,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -7,13 +14,25 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <ListItem>
-      <ListItemAvatar>
-        <Avatar alt="Sams" src={product.pictureUrl} />
-      </ListItemAvatar>
-      <ListItemText>
-        {product.name} - ${product.price.toFixed(2)}
-      </ListItemText>
-    </ListItem>
+    <Card>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="http://picsum.photos/200"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
